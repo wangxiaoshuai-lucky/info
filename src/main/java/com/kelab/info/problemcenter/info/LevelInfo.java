5710cd54e924d0f6727a61a7218facca9a14d00b
wangzy <1510460325@qq.com>
2020-03-27 20:07:52 +0800

    feat: 添加段位的接口定义

---
+++ src/main/java/com/kelab/info/problemcenter/info/LevelInfo.java
@@ -0,0 +1,33 @@
+package com.kelab.info.problemcenter.info;
+
+public class LevelInfo {
+    private Integer id;
+
+    private String name;
+
+    private Integer grade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+}
---
+++ src/main/java/com/kelab/info/problemcenter/info/LevelProblemInfo.java
@@ -0,0 +1,43 @@
+package com.kelab.info.problemcenter.info;
+
+public class LevelProblemInfo {
+    private Integer id;
+
+    private Integer proId;
+
+    private Integer levelId;
+
+    private Integer grade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProId() {
+        return proId;
+    }
+
+    public void setProId(Integer proId) {
+        this.proId = proId;
+    }
+
+    public Integer getLevelId() {
+        return levelId;
+    }
+
+    public void setLevelId(Integer levelId) {
+        this.levelId = levelId;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+}
