62359ad03b13aead4a911d8062c50f9cc67012d9
wangzy <1510460325@qq.com>
2020-04-11 17:38:33 +0800

    feat: 判题数据的管理

---
+++ src/main/java/com/kelab/info/problemcenter/info/ProblemTestDataInfo.java
@@ -0,0 +1,44 @@
+package com.kelab.info.problemcenter.info;
+
+public class ProblemTestDataInfo {
+
+    private Integer id;
+
+    private Integer problemId;
+
+    private String in;
+
+    private String out;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProblemId() {
+        return problemId;
+    }
+
+    public void setProblemId(Integer problemId) {
+        this.problemId = problemId;
+    }
+
+    public String getIn() {
+        return in;
+    }
+
+    public void setIn(String in) {
+        this.in = in;
+    }
+
+    public String getOut() {
+        return out;
+    }
+
+    public void setOut(String out) {
+        this.out = out;
+    }
+}
