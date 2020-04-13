3db5ce8c22a5520964b98cf4dc76ca8d306854da
wangzy <1510460325@qq.com>
2020-04-13 22:22:33 +0800

    feat: 更新对象

---
+++ src/main/java/com/kelab/info/problemcenter/vo/JudgeResult.java
@@ -0,0 +1,34 @@
+package com.kelab.info.problemcenter.vo;
+
+import java.util.List;
+
+public class JudgeResult {
+
+    private String globalMsg;
+
+    private List<ResultCase> result;
+
+	public JudgeResult() {
+	}
+
+	public JudgeResult(String globalMsg, List<ResultCase> result) {
+		this.globalMsg = globalMsg;
+		this.result = result;
+	}
+
+	public String getGlobalMsg() {
+        return globalMsg;
+    }
+
+    public void setGlobalMsg(String globalMsg) {
+        this.globalMsg = globalMsg;
+    }
+
+    public List<ResultCase> getResult() {
+        return result;
+    }
+
+    public void setResult(List<ResultCase> result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/com/kelab/info/problemcenter/vo/ResultCase.java
@@ -0,0 +1,54 @@
+package com.kelab.info.problemcenter.vo;
+
+public class ResultCase {
+
+    private Integer status;
+
+    private Integer timeUsed;
+
+    private Integer memoryUsed;
+
+    private String errorMessage;
+
+	public ResultCase(Integer status, Integer timeUsed, Integer memoryUsed, String errorMessage) {
+		this.status = status;
+		this.timeUsed = timeUsed;
+		this.memoryUsed = memoryUsed;
+		this.errorMessage = errorMessage;
+	}
+
+    public ResultCase() {
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getTimeUsed() {
+        return timeUsed;
+    }
+
+    public void setTimeUsed(Integer timeUsed) {
+        this.timeUsed = timeUsed;
+    }
+
+    public Integer getMemoryUsed() {
+        return memoryUsed;
+    }
+
+    public void setMemoryUsed(Integer memoryUsed) {
+        this.memoryUsed = memoryUsed;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
