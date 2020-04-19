85806f6162767938c9bd95a74b703027e1003a73
wangzy <1510460325@qq.com>
2020-04-19 16:32:35 +0800

    feat: 添加聊天区

---
+++ src/main/java/com/kelab/info/experiment/info/ExperimentChatInfo.java
@@ -0,0 +1,86 @@
+package com.kelab.info.experiment.info;
+
+import com.kelab.info.usercenter.info.UserInfo;
+
+public class ExperimentChatInfo {
+
+    private Integer id;
+
+    private String title;
+
+    private String classId;
+
+    private String content;
+
+    private Integer userId;
+
+    private Long postTime;
+
+    private Integer parentId;
+
+    private UserInfo userInfo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Long getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(Long postTime) {
+        this.postTime = postTime;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+}
---
+++ src/main/java/com/kelab/info/experiment/query/ExperimentChatQuery.java
@@ -0,0 +1,16 @@
+package com.kelab.info.experiment.query;
+
+import com.kelab.info.base.query.BaseQuery;
+
+public class ExperimentChatQuery extends BaseQuery {
+
+    private Integer classId;
+
+    public Integer getClassId() {
+        return classId;
+    }
+
+    public void setClassId(Integer classId) {
+        this.classId = classId;
+    }
+}
